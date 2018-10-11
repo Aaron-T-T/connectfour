08063f8462d81d8f1f34f700b28881ca7945e723
Nathan Black <2210262@flourbluffschools.org>
2018-10-11 14:04:36 -0500

    displayBoard() and emptyBoard() created

--- board.py
+++ board.py
@@ -2,18 +2,28 @@
 
 class Board:
     def __init__(self, width, height):
-        self.bd = [["*"] * width for i in range(height)]
+        self.width = width
+        self.height = height
+        self.bd = [["O"] * width for i in range(height)]
         
     def displayBoard(self):
         for row in self.bd:
             for element in row:
-                print(element,end=' ')
-        print()
+                print(str(element),end=' ')
+            print()
+            
+    def emptyBoard(self):
+        for i in range(self.height):
+            for j in range(len(self.bd[0])):
+                self.bd[i][j] = "*"
             
         
         
         
         
         
-if __name == "__main__":
-    pass
+if __name__ == "__main__":
+    eyy = Board(7, 5)
+    eyy.displayBoard()
+    eyy.emptyBoard()
+    eyy.displayBoard()
