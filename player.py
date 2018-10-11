6c558c617975856b50eec340aae5b460833a999b
TORRES.AARON <2210153@fbisd.flourbluffschools.net>
2018-10-11 13:58:31 -0500

    Skeleton

---
+++ player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self,piece):
+        self.name = self.get_name()
+        self.choice = self.get_choice
+    
+    def get_name(self):
+        pass
+    
+    def get_choice(self):
+        pass
+        
+        
+        
+
+
+if __name__ == "__main__":
+    pass
