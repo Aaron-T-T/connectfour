ede558717e77aa7b9401628490ce2ad6b90dac00
Nathan Black <2210262@flourbluffschools.org>
2018-10-11 13:53:47 -0500

    initial commit x2

--- board.py
+++ board.py
@@ -4,3 +4,16 @@ class Board:
     def __init__(self, width, height):
         self.bd = [["*"] * width for i in range(height)]
         
+    def displayBoard(self):
+        for row in self.bd:
+            for element in row:
+                print(element,end=' ')
+        print()
+            
+        
+        
+        
+        
+        
+if __name == "__main__":
+    pass
