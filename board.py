4284c2e52e6f7c1c942b6029375b154ab3d2dd59
Nathan Black <2210262@flourbluffschools.org>
2018-10-12 14:02:48 -0500

    methods develop

--- board.py
+++ board.py
@@ -16,6 +16,32 @@ class Board:
         for i in range(self.height):
             for j in range(len(self.bd[0])):
                 self.bd[i][j] = "*"
+                
+    def is_full(self):
+        for row in self.bd:
+            for element in row:
+                if element == "O":
+                    return False
+        return True
+    
+    def add_piece(self, piece_string, column):
+        if type(column) != int or int(column) > self.width or int(column) <= 0:
+            raise ValueError("Invalid Column")
+        column = int(column)
+        for i in range(self.height):
+            if self.bd[i][column] != "O":
+                self.bd[i-1][column] = str(piece_string)
+                self.displayBoard()
+                return True
+        raise ValueError("Column Is Full")
+        return False
+        
+    
+        
+    
+    
+                
+    
             
         
         
@@ -25,5 +51,5 @@ class Board:
 if __name__ == "__main__":
     eyy = Board(7, 5)
     eyy.displayBoard()
-    eyy.emptyBoard()
-    eyy.displayBoard()
+    eyy.add_piece("*", 3)
+    eyy.add_piece("*", 3)
